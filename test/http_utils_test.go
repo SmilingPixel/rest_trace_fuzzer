5f3eaee57635bf2949d887dc6804d1a94e7957f0
xunzhou24 <xunzhou24@m.fudan.edu.cn>
2025-02-17 11:29:55 +0800

    test: enhance HTTP and NLP utils tests with additional cases and descriptions

--- test/http_utils_test.go
+++ test/http_utils_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestNewHTTPClient tests the creation of a new HTTP client.
 func TestNewHTTPClient(t *testing.T) {
 	baseURL := "http://example.com"
 	client := utils.NewHTTPClient(baseURL)
@@ -16,6 +17,7 @@ func TestNewHTTPClient(t *testing.T) {
 	assert.Equal(t, baseURL, client.BaseURL)
 }
 
+// TestPerformRequest tests performing a POST request with the HTTP client.
 func TestPerformRequest(t *testing.T) {
 	baseURL := "http://example.com"
 	client := utils.NewHTTPClient(baseURL)
@@ -31,6 +33,7 @@ func TestPerformRequest(t *testing.T) {
 	assert.NotNil(t, respBody)
 }
 
+// TestPerformHTTPSRequest tests performing a POST request with the HTTPS client.
 func TestPerformHTTPSRequest(t *testing.T) {
 	baseURL := "https://example.com"
 	client := utils.NewHTTPClient(baseURL)
@@ -46,6 +49,7 @@ func TestPerformHTTPSRequest(t *testing.T) {
 	assert.NotNil(t, respBody)
 }
 
+// TestPerformRequestWithRetry tests performing a POST request with retries using the HTTP client.
 func TestPerformRequestWithRetry(t *testing.T) {
 	baseURL := "http://example.com"
 	client := utils.NewHTTPClient(baseURL)
@@ -61,6 +65,7 @@ func TestPerformRequestWithRetry(t *testing.T) {
 	assert.NotNil(t, respBody)
 }
 
+// TestPerformGet tests performing a GET request with the HTTP client.
 func TestPerformGet(t *testing.T) {
 	baseURL := "http://example.com"
 	client := utils.NewHTTPClient(baseURL)
@@ -75,6 +80,7 @@ func TestPerformGet(t *testing.T) {
 	assert.NotNil(t, respBody)
 }
 
+// TestGetStatusCodeClass tests the classification of HTTP status codes.
 func TestGetStatusCodeClass(t *testing.T) {
 	assert.Equal(t, consts.StatusContinue, utils.GetStatusCodeClass(consts.StatusContinue))
 	assert.Equal(t, consts.StatusOK, utils.GetStatusCodeClass(consts.StatusOK))
@@ -84,6 +90,7 @@ func TestGetStatusCodeClass(t *testing.T) {
 	assert.Equal(t, -1, utils.GetStatusCodeClass(999))
 }
 
+// TestIsStatusCodeSuccess tests if the given status code is a success status code.
 func TestIsStatusCodeSuccess(t *testing.T) {
 	assert.True(t, utils.IsStatusCodeSuccess(consts.StatusOK))
 	assert.False(t, utils.IsStatusCodeSuccess(consts.StatusBadRequest))
